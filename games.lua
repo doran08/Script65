43e2a70c583de0921343fc4bebbe2c85a13fbee0
doran08 <95293707+doran08@users.noreply.github.com>
2022-10-03 20:02:53 +0200

    Update games.lua

--- games.lua
+++ games.lua
@@ -3,7 +3,7 @@ local url = "https://raw.githubusercontent.com/doran08/Script65/main/Games"
 
 local games = {
     [3652625463] = "Lifting simulator";
-    [10239643962] = "explosion simulator";
+    [10239643962] = "Explosion simulator";
 }
 
 for i,v in next, games do
